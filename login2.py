a7eb52774a195ea63813cced2e3f62b1f88a8aaf
minkyu_park <torosa123456@gmail.com>
2025-01-05 21:22:46 +0900

    Create login2.py

---
+++ login2.py
@@ -0,0 +1,13 @@
+def solution(id_pw, db):
+    answer = 'fail'
+    if id_pw in db:
+        answer = 'login'
+    else:
+        for i in db:
+            if id_pw[0] in i:
+                answer = 'wrong pw'
+
+    print(answer)
+    return answer
+
+solution(["meosseugi", "1234"],[["rardss", "123"], ["yyoom", "12345"]])
