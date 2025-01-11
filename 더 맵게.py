12598002999b697b6f6a12f6659c8a3a35b6a525
minkyu_park <torosa123456@gmail.com>
2025-01-11 18:54:59 +0900

    Update 더 맵게.py

--- 더 맵게.py
+++ 더 맵게.py
@@ -6,9 +6,12 @@ Leo는 모든 음식의 스코빌 지수가 K 이상이 될 때까지 반복하�
 Leo가 가진 음식의 스코빌 지수를 담은 배열 scoville과 원하는 스코빌 지수 K가 주어질 때, 모든 음식의 스코빌 지수를 K 이상으로 만들기 위해 섞어야 하는 최소 횟수를 return 하도록 solution 함수를 작성해주세요.
 '''
 
+import heapq
+
 def solution(scoville, K):
     answer = 0
     num = scoville
+    heapq.heapify(scoville)
     while (min(scoville) < K):
         scoville.sort()
         scoville[1] = scoville[0] + scoville[1] * 2
