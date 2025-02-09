bc85f7e91bab03cfd0196bd5620a89961a8a7858
minkyu_park <torosa123456@gmail.com>
2025-02-09 23:36:09 +0900

    Update login.py

--- login.py
+++ login.py
@@ -7,7 +7,6 @@ def solution(id_pw, db):
             if id_pw[0] in i:
                 answer = 'wrong pw'
 
-    print(answer)
     return answer
 
 solution(["meosseugi", "1234"],[["rardss", "123"], ["yyoom", "1234"], ["meosseugi", "1234"]])
